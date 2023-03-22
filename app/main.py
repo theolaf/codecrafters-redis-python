2e2afecaaba8f092af82248e2fe6fb3dbc60d63e
Theo Lafforgue <theo.lafforgue@arbounie.nl>
2023-03-22 15:58:00 +0100

    phase 5 try 1

--- app/main.py
+++ app/main.py
@@ -1,19 +1,38 @@
 import socket
 import threading
 
+def parse_response(msg):
+    decoded = msg.decode("utf-8")
+    if len(decoded) == 0:
+        return None
+    return decoded.split('\r\n')
+
+def write_reply(input):
+    if input is None:
+        return None
+    command = input[2].lower()
+    if command == "ping":
+        return "+PONG\r\n"
+    if command == "echo":
+        return f"${len(input[4])}\r\n{input[4]}\r\n"
+    else:
+        return None
+
 def handle_connection(con):
     while True:
         try:
-            con.recv(1024)  # wait for client to send data
-            con.send(b"+PONG\r\n")
+            msg = con.recv(1024)
+            resp = parse_response(msg)
+            reply = write_reply(resp)
+            if reply is not None:
+                con.send(str.encode(reply))
         except ConnectionError:
             break
-    con.close()
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
-        connection, address = server_socket.accept()  # wait for client
+        connection, address = server_socket.accept()
         print(f"Connected to {address}")
         threading.Thread(target=handle_connection, args=[connection]).start()
 
