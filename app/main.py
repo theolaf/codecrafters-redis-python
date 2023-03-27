68537c6b457c1d02b6cc21d677f91b7902903512
Theo Lafforgue <theo.lafforgue@arbounie.nl>
2023-03-27 14:39:33 +0200

    refactored most of the code into a server class

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import socket
 import threading
 
-class RedisHandler:
-    def __init__(self):
+class RedisServer:
+    # constructor, getters and setters
+    def __init__(self, address=(("localhost", 6379))):
         self.db = {}
+        self.server_socket = socket.create_server(address, reuse_port=True)
     
     def set(self, key, value):
         self.db[key] = value
@@ -12,48 +14,51 @@ class RedisHandler:
     def get(self, key):
         return self.db.get(key)
     
-def parse_response(msg):
-    decoded = msg.decode("utf-8")
-    if len(decoded) == 0:
-        return None
-    return decoded.split('\r\n')
+    # connection handling
+    def event_loop(self):
+        while True:
+            connection, address = self.server_socket.accept()
+            print(f"Connected to {address}")
+            threading.Thread(target=self.handle_connection, args=[connection]).start()
 
-def write_reply(input, redis):
-    if input is None:
-        return None
-    command = input[2].lower()
-    if command == "ping":
-        return "+PONG\r\n"
-    if command == "echo":
-        return f"${len(input[4])}\r\n{input[4]}\r\n"
-    if command == "set":
-        redis.set(input[4], input[6])
-        return "+OK\r\n"
-    if command == "get":
-        value = redis.get(input[4])
-        return f"${len(value)}\r\n{value}\r\n" if value else "$-1\r\n"
-    else:
-        return None
+    def handle_connection(self, con):
+        while True:
+            try:
+                msg = con.recv(1024)
+                resp = self.parse_response(msg)
+                reply = self.write_reply(resp)
+                if reply is not None:
+                    con.send(str.encode(reply))
+            except ConnectionError:
+                break
+    
+    # parsing and response logic
+    def parse_response(self, msg):
+        decoded = msg.decode("utf-8")
+        if len(decoded) == 0:
+            return None
+        return decoded.split('\r\n')
 
-def handle_connection(con, redis):
-    while True:
-        try:
-            msg = con.recv(1024)
-            resp = parse_response(msg)
-            reply = write_reply(resp, redis)
-            if reply is not None:
-                con.send(str.encode(reply))
-        except ConnectionError:
-            break
+    def write_reply(self, input):
+        if input is None:
+            return None
+        command = input[2].lower()
+        if command == "ping":
+            return "+PONG\r\n"
+        if command == "echo":
+            return f"${len(input[4])}\r\n{input[4]}\r\n"
+        if command == "set":
+            self.set(input[4], input[6])
+            return "+OK\r\n"
+        if command == "get":
+            value = self.get(input[4])
+            return f"${len(value)}\r\n{value}\r\n" if value else "$-1\r\n"
+        else:
+            return None
 
 def main():
-    redis = RedisHandler()
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        connection, address = server_socket.accept()
-        print(f"Connected to {address}")
-        threading.Thread(target=handle_connection, args=[connection, redis]).start()
-
+    redis = RedisServer()
+    redis.event_loop()
 
 if __name__ == "__main__":
     main()
